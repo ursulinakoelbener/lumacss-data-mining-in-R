73becfcc1cfee72e39487180d020df33d417893c
Ursulina Kölbener <ursulina@koelbener.ch>
2023-03-08 14:47:15 +0100

    load data from webpage

--- World Heritage Sites in Danger.R
+++ World Heritage Sites in Danger.R
@@ -1,8 +1,14 @@
-install.packages("stringr")
-install.packages("XML")
-install.packages("maps")
+# install.packages("stringr")
+# install.packages("XML")
+# install.packages("maps")
 
 # load packages
 library(stringr)
 library(XML)
 library(maps)
+
+# load data from webpage
+heritage_parsed <- htmlParse("https://en.wikipedia.org/wiki/List_of_World_Heritage_in_Danger", encoding = "UTF-8")
+# Warning message: XML content does not seem to be XML: ''
+
+tables <- readHTMLTable(heritage_parsed, stringAsFactors = FALSE)
