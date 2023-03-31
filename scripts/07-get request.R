6aa120684c5fa64a4694c3d3f2a64927ecaf6cf5
Ursulina Kölbener <ursulina@koelbener.ch>
2023-03-31 14:05:17 +0200

    use rvest and httr

--- scripts/07-get request.R
+++ scripts/07-get request.R
@@ -12,4 +12,15 @@ news <- read_html(url)%>%
 # scraping with httr
 url <- "https://www.unilu.ch/"
 
-headlines <- GET(url) # this is a response from the server
+resp <- GET(url) # this is a response from the server
+
+# access content
+httr::content(resp)
+
+# access protocol
+httr::http_status(resp)
+
+# how to actually use the status
+if(http_status(resp)$category == "Success"){
+  content(resp)
+}
