1d97721080b1610c0f9cbaa54533f7ed90b7daab
Ursulina Kölbener <ursulina@koelbener.ch>
2023-04-07 15:07:49 +0200

    adds examples

--- scripts/08-regular expressions.R
+++ scripts/08-regular expressions.R
@@ -1,5 +1,6 @@
 # load packages
 library(tidyverse)
+library(stringr)
 
 # use cat to escape
 cat("Escaping: \\")
@@ -33,3 +34,18 @@ string <- str_c("file", 1:10, ".csv")
 
 # all files with the ending ".csv"
 str_view_all(string, pattern = "\\.csv$") # $ to match at the end
+
+# presenation ist not the same as the string
+x <- c("\"", "\\")
+x
+writeLines(x)
+
+y <- "\u00b5"
+y
+
+# number of characters in a string
+str_length(y)
+
+# subsetting strings
+z <- c("Apple", "Banana", "Pear")
+str_sub(z, 1, 3)
